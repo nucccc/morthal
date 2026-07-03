25f5b3875d2cf7b48c132bf7ba22eb86e396f063
nucccc <dan.dom.nuc@gmail.com>
2026-07-03 13:10:09 +0200

    fixed small thing in codebase tests

--- tests/test_utils_codebase.py
+++ tests/test_utils_codebase.py
@@ -13,5 +13,5 @@ from morthal.utils.codebase import LocalCodebase
 def test_local_codebase(tmp_path):
     lc = LocalCodebase(path=Path(tmp_path))
     
-    assert lc.repo_path == Path(tmp_path)
+    assert lc.path == Path(tmp_path)
     assert lc.name == str(tmp_path)
